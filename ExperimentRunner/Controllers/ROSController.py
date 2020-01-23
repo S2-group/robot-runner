d87c640d6cb25cd9c7d08e0f8b877e67d64ca9a3
Stan Swanborn <stanswanborn@gmail.com>
2020-01-23 22:25:27 +0100

    Automated gazebo launch and stop based on ROS interaction. WIP: Wait for graceful shutdown of launch file

--- ExperimentRunner/Controllers/ROSController.py
+++ ExperimentRunner/Controllers/ROSController.py
@@ -3,6 +3,7 @@ import rospy
 import signal
 import subprocess
 from pathlib import Path
+from std_msgs.msg import Bool
 from ExperimentRunner.Utilities.RobotRunnerOutput import RobotRunnerOutput as output
 
 
@@ -11,9 +12,15 @@ class ROSController:
 
     def start_fresh_run(self, launch_file: Path): # add log_file to output roslaunch pipe to
         try:
+            # TODO: Make sure /tmp/robot_runner exists
             FNULL = open(os.devnull, 'w')
-            subprocess.call(f"roslaunch {launch_file}", shell=True, stdout=FNULL, stderr=subprocess.STDOUT)
-            output.console_log("Succesfully started launch file")
+            self.process = subprocess.Popen(
+                                f"roslaunch --pid=/tmp/robot_runner/roslaunch.pid {launch_file}",
+                                shell=True, stdout=FNULL, stderr=subprocess.STDOUT
+                            )
+
+            # TODO: Wait for ROS Master to be up and running
+            # TODO: In case of sim: wait for Gazebo
             self.listen_for_run_completion()
         except Exception as e:
             output.console_log(e)
@@ -21,19 +28,29 @@ class ROSController:
     def listen_for_run_completion(self):
         try:
             rospy.init_node("robot_runner")
-            self.run_sub = rospy.Subscriber("/run_completed", self.run_completed)
+            self.run_sub = rospy.Subscriber("/robot_runner/run_completed", Bool, self.run_completed)
 
-            rospy.on_shutdown(self.ros_shutdown())
-            rospy.spin()
+            # rospy.on_shutdown(self.ros_shutdown())
+            while not rospy.is_shutdown():
+                output.console_log_loading_animated("Waiting for run to complete...")
+
+            self.ros_shutdown()
         except:
             output.console_log("Could not initialise ROS node...")
 
-    def run_completed(self):
-        output.console_log("Run completed!")
-        rospy.signal_shutdown("Run completed")
+    def run_completed(self, completed: Bool):
+        if completed:
+            output.console_log("Run completed")
+            rospy.signal_shutdown("Run completed")
 
     def ros_shutdown(self):
+        # TODO: Change from 'terminated' to: 'terminating...' and wait for
+        # TODO: actual confirmation everything has closed down gracefully
         self.run_sub.unregister()
         output.console_log("ROS Node terminated...")
-        # self.process.send_signal(signal.SIGINT)
+        self.process.send_signal(signal.SIGINT)
+        with open('/tmp/robot_runner/roslaunch.pid', 'r') as myfile:
+            pid = int(myfile.read())
+            os.kill(pid, signal.SIGINT)
+
         output.console_log("Experiment run instance (roslaunch) terminated")
--- ExperimentRunner/Utilities/RobotRunnerOutput.py
+++ ExperimentRunner/Utilities/RobotRunnerOutput.py
@@ -1,4 +1,18 @@
+import time
+
+
 class RobotRunnerOutput:
+    animation = "|/-\\"
+    idx = 0
+
     @staticmethod
     def console_log(str):
         print(f"[ROBOT_RUNNER]: {str}")
+
+    @staticmethod
+    def console_log_loading_animated(str):
+        idx = RobotRunnerOutput.idx
+        animation = RobotRunnerOutput.animation
+        print(f"{str} {animation[idx % len(animation)]}", end="\r")
+        RobotRunnerOutput.idx += 1
+        time.sleep(0.1)
